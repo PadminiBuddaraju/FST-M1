a7b7ac304be9fc4593ad219bc2156ed96fa6f3cf
PadminiBuddaraju <Buddaraju.Padmini.Satya.Bangaramma@ibm.com>
2024-07-03 12:05:26 +0530

    Delete TestNG/Activities/Activity4_suiteExample directory

--- TestNG/Activities/Activity4_suiteExample/DemoTwo.java
+++
@@ -1,9 +0,0 @@
-package Activity4_suiteExample;
-import org.testng.annotations.Test;
-
-public class DemoTwo {
-    @Test
-    public void TestCase() {
-        System.out.println("I'm in the test case from DemoTwo Class");
-    }
-}
